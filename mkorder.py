2f2db9443770a21f96d30b6b989f1819489cb21b
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-07-15 16:21:46 -0500

    add mkorder.py

---
+++ mkorder.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import sys
+import os 
+from datetime import date
+
+# Get today's date
+today = date.today()
+# today = str(today)
+today = today.strftime("%b-%d-%Y")
+
+
+# Path to template
+blank_form_path = r"/c/Users/nmark/Documents/NUSTARS/order_template.xlsx"
+
+# Get command line input
+x = sys.argv
+
+# Make sure there are enough inputs
+if len(x) != 2:
+    print('Must enter new form name as input')
+    quit()
+
+# Combine the string and create the cmd
+name = x[1]
+cmd = "cp" + " {}".format(blank_form_path) + " {}".format(today+"_"+name) + ".xlsx"
+os.system(cmd)
