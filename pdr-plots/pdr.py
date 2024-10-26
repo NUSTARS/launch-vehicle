60c80eead057c211a6c4f6624d7078fda0f5956f
max-hug <maxhug@u.northwestern.edu>
2024-10-26 09:55:10 -0500

    fixed labeling

--- pdr-plots/pdr.py
+++ pdr-plots/pdr.py
@@ -23,26 +23,30 @@ t_off_rail = 0.3
 def format_ax(ax):
     pass
 
-def standard_plot(dfs, names, x_field, y_field):
+def standard_plot(dfs, file_names, fields, title):
     fig, ax = plt.subplots()
     for df in dfs:
-        ax.plot(df[x_field], df[y_field])
-    ax.set_title(y_field + ' for Possible Flight Paths')
-    ax.set_xlabel(x_field)
-    ax.set_ylabel(y_field)
-    ax.legend(names)
+        ax.plot(df[fields[0]], df[fields[1]])
+    ax.set_title(title + ' for Possible Flight Paths')
+    ax.set_xlabel(fields[0].replace("(", "[").replace(")", "]"))
+    ax.set_ylabel(fields[1].replace("(", "[").replace(")", "]"))
+    ax.legend(file_names)
     # DO NOT SHOW PLOT UNTIL THE MAIN FUNCTION
 
-def clipped_plot(dfs, names, x_field, y_field, start_time, end_time):
+    return fig
+
+def clipped_plot(dfs, file_names, fields, title, times):
     fig, ax = plt.subplots()
     for df in dfs:
-        df = df.loc[(df[x_field] >= start_time) & (df[x_field] <= end_time)]
-        ax.plot(df[x_field], df[y_field])
-    ax.set_title(y_field + ' for Possible Flight Paths')
-    ax.set_xlabel(x_field)
-    ax.set_ylabel(y_field)
-    ax.legend(names)
+        df = df.loc[(df[fields[0]] >= times[0]) & (df[fields[0]] <= times[1])]
+        ax.plot(df[fields[0]], df[fields[1]])
+    ax.set_title(title + ' for Possible Flight Paths')
+    ax.set_xlabel(fields[0].replace("(", "[").replace(")", "]"))
+    ax.set_ylabel(fields[1].replace("(", "[").replace(")", "]"))
+    ax.legend(file_names)
     # DO NOT SHOW PLOT UNTIL THE MAIN FUNCTION
+
+    return fig
 
 def main():
     print("Hello, World!")
@@ -54,14 +58,15 @@ def main():
         dfs.append(pd.read_csv(data_dir / f"{file_name}.csv", comment='#'))
         print(file_name + " Loaded")
 
-    standard_plot(dfs, file_names, 'Time (s)', 'Altitude (ft)')
-    standard_plot(dfs, file_names, 'Time (s)', 'Vertical velocity (ft/s)')
-    standard_plot(dfs, file_names, 'Time (s)', 'Vertical acceleration (ft/s²)')
-    clipped_plot(dfs, file_names, 'Time (s)', 'Stability margin calibers (​)', t_off_rail, 15)
+    fig = standard_plot(dfs, file_names, ['Time (s)', 'Altitude (ft)'], "Height AGL")
+    fig.axes[0].set_ylabel('Height AGL [ft]')
+    standard_plot(dfs, file_names, ['Time (s)', 'Vertical velocity (ft/s)'], "Velocity")
+    standard_plot(dfs, file_names, ['Time (s)', 'Vertical acceleration (ft/s²)'], "Acceleration")
+    clipped_plot(dfs, file_names, ['Time (s)', 'Stability margin calibers (​)'], "Dynamic Stability", [t_off_rail, 15])
 
     if save_figures:
         for i in plt.get_fignums():
-            plt.figure(i).savefig(project_root / f'figures/figure_{i}.png', dpi=300)
+            plt.figure(i).savefig(project_root / f'figures/{plt.figure(i).axes[0].get_title().replace(" ", "_")}.png', dpi=300)
 
     plt.show()
 
