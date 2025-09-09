635ab399e01959a3bf830e8a5cec94f32065e582
Bernie Waldman <berniewaldman2027@u.northwestern.edu>
2025-09-09 14:30:45 -0500

    Plot drag area and drag force v time

--- analysis/RecoveryDynamics.py
+++ analysis/RecoveryDynamics.py
@@ -1,17 +1,27 @@
 import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
+import mplcursors
 
 # PYTHON 3.7
 
 # USER INPUTS
-weight = 36.56 + 3.73 # lbs
-apogee = 5000  # ft
-drogue_d = 18  # in
+# weight = 36.56 + 3.73 # lbs
+# apogee = 5000  # ft
+# drogue_d = 18  # in
+# main_cd = 2.2
+# drogue_cd = 1.6
+# main_alt = 550  # ft
+# main_d = 120  # in
+# airframe_d = 4  # in
+
+weight = 10.99 # lbs
+apogee = 4000  # ft
+drogue_d = 18.48  # in
 main_cd = 2.2
-drogue_cd = 1.6
+drogue_cd = 0.9
 main_alt = 550  # ft
-main_d = 120  # in
+main_d = 96  # in
 airframe_d = 4  # in
 
 # CONSTANTS
@@ -35,6 +45,7 @@ params = {
     "mass": mass,
     "t_fill": None
 }
+
 
 def drogue_dynamics(t, state, params):
     x, y, vx, vy = state  # Unpack state
@@ -61,6 +72,7 @@ def inflating_dynamics(t, state, params):
     # calculate drag    
     f_drag = ((0.5 * rho * vy**2 * params["main_cd"] * computed_area) + 
               (0.5 * rho * vy**2 * params["drogue_cd"] * params["drogue_A"]))
+
     f_y = f_drag - params["mass"] * g
     f_x = 0
 
@@ -120,26 +132,26 @@ landing_event.terminal = True
 landing_event.direction = -1
 
 
-def plot_results(time, states, params):
-    """Plot height and velocity against time."""
-    fig, axs = plt.subplots(2, 1, figsize=(10, 8))
+def plot_results(time, states, drag_area, drag_force, params):
+    """Plot height, velocity, dragarea, and drag force against time."""
+    fig1, motion_axs = plt.subplots(2, 1, figsize=(10, 8))
 
     # Height vs. Time
-    axs[0].plot(time, states[1, :], "-")
-    axs[0].set_ylim(0, params["apogee"]+100)
-    axs[0].set_xlabel("Time [s]")
-    axs[0].set_ylabel("Height [ft]")
-    axs[0].axhline(params["apogee"], linestyle='--', color='b', label='Apogee')
-    axs[0].axhline(params["main_alt"], linestyle='--', color='g', label='Main Deployment')
-    axs[0].set_title("Height vs. Time")
-    axs[0].grid(True)  # Add grid lines
+    altitude, = motion_axs[0].plot(time, states[1, :], "-", label="Height [ft]")
+    motion_axs[0].set_ylim(0, params["apogee"]+100)
+    motion_axs[0].set_xlabel("Time [s]")
+    motion_axs[0].set_ylabel("Height [ft]")
+    motion_axs[0].axhline(params["apogee"], linestyle='--', color='b', label='Apogee')
+    motion_axs[0].axhline(params["main_alt"], linestyle='--', color='g', label='Main Deployment')
+    motion_axs[0].set_title("Height vs. Time")
+    motion_axs[0].grid(True)  # Add grid lines
 
     # Velocity vs. Time
-    axs[1].plot(time, states[3, :], "--r")
-    axs[1].set_xlabel("Time [s]")
-    axs[1].set_ylabel("Velocity [ft/s]")
-    axs[1].set_title("Velocity vs. Time")
-    axs[1].grid(True)  # Add grid lines
+    velocity, = motion_axs[1].plot(time, states[3, :], "--r", label="Velocity [ft/s]")
+    motion_axs[1].set_xlabel("Time [s]")
+    motion_axs[1].set_ylabel("Velocity [ft/s]")
+    motion_axs[1].set_title("Velocity vs. Time")
+    motion_axs[1].grid(True)  # Add grid lines
 
     # Find velocity just before the main parachute opens (at `main_alt`)
     idx_main_open = np.argmax(states[1, :] <= params["main_alt"])  # First index where altitude is below `main_alt`
@@ -151,18 +163,40 @@ def plot_results(time, states, params):
     descent_time = time[-1]
     impact_velocity = states[3, -1]
 
-    opening_force = 0.5 * density(0) * terminal_velocity_drogue**2 * params["main_cd"] * params["main_A"]
-    opening_acceleration =(opening_force-params["mass"]*g)/params["mass"]
+    #opening_force = 0.5 * density(0) * terminal_velocity_drogue**2 * params["main_cd"] * params["main_A"]   # this is the old way
+    #opening_acceleration =(opening_force-params["mass"]*g)/params["mass"]
 
     result_text = (
         f"Terminal Velocity: {terminal_velocity_drogue:.2f} ft/s\n"
         f"Total Descent Time: {descent_time:.2f} s\n"
         f"Main Impact Velocity: {impact_velocity:.2f} ft/s\n"
-        f"Main Opening Load: {opening_force:.2f} lbf\n"
-        f"Main Opening Acceleration: {opening_acceleration:.2f} ft/s^2"
+        #f"Main Opening Load: {opening_force:.2f} lbf\n"
+        #f"Main Opening Acceleration: {opening_acceleration:.2f} ft/s^2"
     )
-    axs[0].text(0.95, 0.9, result_text, fontsize=12, va="top", ha="right",
-            transform=axs[0].transAxes, bbox=dict(boxstyle="round,pad=0.3", edgecolor="none", facecolor="white"))
+    motion_axs[0].text(0.95, 0.9, result_text, fontsize=12, va="top", ha="right",
+            transform=motion_axs[0].transAxes, bbox=dict(boxstyle="round,pad=0.3", edgecolor="none", facecolor="white"))
+
+    # Drag plots
+    fig2, drag_axs = plt.subplots(figsize=(10, 8))
+
+    # Drag area vs. time
+    dragarea_line, = drag_axs.plot(time, drag_area, label="Drag Area")
+    drag_axs.set_xlabel("Time [s]")
+    drag_axs.set_ylabel("Drag Area [ft^2], Drag Force [lbf]")
+    drag_axs.set_title("Drag Area and Force vs. Time")
+    drag_axs.grid(True)
+
+    # find where main is fully open and plot a line there
+    idx_inflated = np.argmax(drag_area == params["main_A"]*params["main_cd"] + params["drogue_A"]*params["drogue_cd"])
+    drag_axs.axvline(time[idx_inflated], linestyle="--", linewidth=0.75, label="Main Parachute Fully Open")
+
+    # Drag force vs. time
+    dragforce_line, = drag_axs.plot(time, drag_force, label="Drag Force")
+    drag_axs.legend()
+    
+    # make hovering over lines display values
+    cursor = mplcursors.cursor([altitude, velocity, dragarea_line, dragforce_line], hover=True)
+
 
     plt.tight_layout()
 
@@ -189,7 +223,8 @@ inflating_sol = solve_ivp(
     drogue_sol.y[:,-1],
     method='RK45',
     events=full_inflation_event,
-    rtol=1e-6
+    rtol=1e-6,
+    max_step=0.01
 )
 
 main_sol = solve_ivp(
@@ -201,9 +236,41 @@ main_sol = solve_ivp(
     rtol=1e-6
 )
 
-combined_time = np.concatenate((drogue_sol.t, (inflating_sol.t + drogue_sol.t[-1]), (main_sol.t + drogue_sol.t[-1] + inflating_sol.t[-1])))
-combined_traj = np.concatenate((drogue_sol.y, inflating_sol.y, main_sol.y), axis=1)
+combined_time = np.concatenate((drogue_sol.t, 
+                                (inflating_sol.t + drogue_sol.t[-1]), 
+                                (main_sol.t + drogue_sol.t[-1] + inflating_sol.t[-1])))
+combined_traj = np.concatenate((drogue_sol.y, 
+                                inflating_sol.y, 
+                                main_sol.y), 
+                                axis=1)
+
+
+# get drag area and drag force v time for whole flight
+density_vec = np.vectorize(density)   # needed for vectorized operation
+
+drogue_dragarea = np.repeat(params["drogue_A"]*params["drogue_cd"], len(drogue_sol.t))
+drogue_dragforce = 0.5 * density_vec(drogue_sol.y[1]) * (drogue_sol.y[3])**2 * drogue_dragarea
+
+# I couldn't save data on parachute inflation/drag force bc of how this solver works
+# just calculate area (we have t_fill and velocities) and drag force using vectorized operations here, then plot
+inflation_curve = ((1-eta)*(inflating_sol.t / params["t_fill"])**3 + eta)**2
+inflating_dragarea = inflation_curve * params["main_A"] * params["main_cd"] + drogue_dragarea[-1]
+inflating_dragforce = 0.5 * density(params["main_alt"]) * (inflating_sol.y[3])**2 * inflating_dragarea
+
+main_dragarea = np.repeat(params["main_A"]*params["main_cd"] + params["drogue_A"]*params["drogue_cd"], len(main_sol.t))
+main_dragforce = 0.5 * density_vec(main_sol.y[1]) * (main_sol.y[3])**2 * main_dragarea
+
+# combine
+combined_dragarea = np.concatenate((drogue_dragarea,
+                                   inflating_dragarea,
+                                   main_dragarea))
+
+combined_dragforce = np.concatenate((drogue_dragforce,
+                                  inflating_dragforce,
+                                  main_dragforce))
+
 
 # Plot the results
-plot_results(combined_time, combined_traj, params)
+plot_results(combined_time, combined_traj, combined_dragarea, combined_dragforce, params)
+
 plt.show()
